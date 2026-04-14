b7c283274c8b672d7969a1cb6c6352b05bdc49c0
Developer <dev@example.com>
2026-04-14 19:51:17 +0800

    Initial commit: Add backend and frontend code

---
+++ Backend/src/main/java/com/example/pesolend/PesolendApplication.java
@@ -0,0 +1,13 @@
+package com.example.pesolend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesolendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesolendApplication.class, args);
+	}
+
+}
