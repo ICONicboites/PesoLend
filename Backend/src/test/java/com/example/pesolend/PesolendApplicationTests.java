a80eaa211b00922d3400655692372f2562c878ab
Developer <dev@example.com>
2026-04-14 19:51:17 +0800

    Initial commit: Add backend and frontend code

---
+++ Backend/src/test/java/com/example/pesolend/PesolendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pesolend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesolendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
